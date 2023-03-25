55389a01131e07abd86548e93f0e27f5625811d8
kisho <kisho@LAPTOP-27LCBCLL>
2023-03-25 17:02:14 -0500

    initial commit with sample endpoints

---
+++ src/main/java/com/sample/student/configuration/ProductionOnlyConfiguration.java
@@ -0,0 +1,13 @@
+package com.sample.student.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@Profile("prod")
+public class ProductionOnlyConfiguration {
+
+    @Bean
+    public String dummy() {
+        return "something";
+    }
+}
---
+++ src/main/java/com/sample/student/model/Course.java
@@ -0,0 +1,10 @@
+package com.sample.student.model;
+
+import java.util.List;
+
+public record Course(String id,
+                     String name,
+                     String description,
+                     List<String> steps) {
+
+}
